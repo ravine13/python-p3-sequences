f8743a428d9bc4eaa435f7874643a2f753068611
ravine13 <kuriaderek@gmail.com>
2023-11-22 21:52:58 +0300

    passed test

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
 
-def print_fibonacci(length):
+def print_fibonacci(length=None):
     fibonacci_list = []
 
-    if length > 0:
-        fibonacci_list.append(0)
-        if length > 1:
-            fibonacci_list.append(1)
-            if length > 2:
-                i = 2
-                while i < length:
-                    value = fibonacci_list[i-1] + fibonacci_list[i-2]
-                    fibonacci_list.append(value)
-                    i += 1
+    if length == 0:
+        print ([])
+    elif length == 1:
+        print('[0]')
+    else:
+        fibonacci_list = [0, 1]
+        if length > 2:
+            i = 2
+            while i < length:
+                value = fibonacci_list[i - 1] + fibonacci_list[i - 2]
+                fibonacci_list.append(value)
+                i += 1
 
-    print(fibonacci_list)
+        print(fibonacci_list)
